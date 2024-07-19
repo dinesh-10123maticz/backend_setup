065e134c6b10c9d9a992f9858e6147139415c9ab
dinesh-10123maticz <dineshbabumaticz@gmail.com>
2024-07-19 11:58:36 +0530

    Create constants.js

---
+++ config/constants.js
@@ -0,0 +1,13 @@
+
+/* Schemas */
+   //eg : import Userschema from './' ;
+
+/* Define Database collection Name */
+
+export const tables = Object.freeze({
+    USERTABLE : "user"
+})
+
+export const schemas = Object.freeze({
+    USERSCHEMA : "" //Userschema
+})
