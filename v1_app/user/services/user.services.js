c81e0db73378a4a299e6c6078408d266a31edc98
dinesh-10123maticz <dineshbabumaticz@gmail.com>
2024-07-19 19:18:31 +0530

    Create user.services.js

---
+++ v1_app/user/services/user.services.js
@@ -0,0 +1,18 @@
+/* Schemas */
+// // eg
+// import User from '../schema/user.schema';
+
+/* Mongoose Helpers */
+// import { Find ,Findone } from '../../../helper/mongoosehelper';
+
+// export const userFind = async (data) =>{
+//     let retData;
+//     retData = await Find({...data,table : User})
+//     return retData;
+// }
+
+// export const userFindOne = async (data) =>{
+//     let retData;
+//     retData = await Findone({...data,table : User})
+//     return retData;
+// }
